ee3298359b6908329ec5265c0308109c93e10621
George Arredondo <george.arredondo87@gmail.com>
2020-07-20 14:17:41 -0500

    SQL tables_exercise

---
+++ tables_exercise.sql
@@ -0,0 +1,11 @@
+# Database 
+USE `employees`;
+# Showing tables in database
+SHOW TABLES;
+# Showing employees table
+#5 employees table shows employee #, DOB, First Name, Last Name, gender and hire date
+#6 Employee #
+#7 First Name, Last Name
+#8 DOB, Hire Date
+#9 Department # and department name
+SHOW `dept_manager`;
